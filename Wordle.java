216aac218af22bc6e20d21de778231a1c24ef49e
Akhil841 <44041958+Akhil841@users.noreply.github.com>
2022-02-11 21:58:48 -0800

    Commented the code of Wordle and WordleTools

--- Wordle.java
+++ Wordle.java
@@ -2,63 +2,112 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/** 
+ * 
+ * The main, executable portion of the Wordle clone. This is the entrypoint used by Wordle.jar.
+ * 
+ * Written by Akhil Pillai on 11 February 2022
+*/
 public class Wordle {
     public static void main(String[] args) {
+        //The length of the words we want to guess
         final int WORD_LENGTH = 5;
+        //Store all possible words
         ArrayList<String> words = new ArrayList<>();
+        //This is in a try-catch since you have to handle IOExceptions
+        //if you try to read a file using Files.readAllLines()
         try {
+            //Get all the words in the dictionary, 
+            //filter out the words of length WORD_LENGTH, 
+            //and convert them all to uppercase
+            //to simplify verification.
             words = WordleTools.allToUpperCase(WordleTools.getNLetterWords(WordleTools.readFile("dictionary.txt"), WORD_LENGTH));
         }
+        //If the dictionary is missing from the current directory
         catch (IOException e) {
+            //Notify the user
             System.out.println("FATAL: Missing dictionary");
+            //Terminate the program with an exit code of 1
+            //since the program was unable to run properly
             System.exit(1);
         }
+        //store whether the user found the correct word here
         boolean found = false;
+        //The number of lives the user starts with
         final int MAX_LIVES = 6;
+        //Initialize life counter with the maximum number
+        //of lives they can have
         int lives = MAX_LIVES;
+        //Scanner for taking user input
         Scanner input = new Scanner(System.in);
+        //Pick a random word from the list of words and convert it to uppercase.
         String correct = WordleTools.pickRandom(words).toUpperCase();
+        //While the user hasn't found the word and still has lives
         while (!found && lives != 0) {
+            //Ask for user's guess
             System.out.println("Enter your guess. Lives remaining: " + lives);
+            //Convert to uppercase to simplify verification
             String guess = input.next().toUpperCase();
+            //User must enter a 5-letter word
             if (guess.length() != 5) {
                 System.out.println("Please enter a 5-letter word");
                 continue;
             }
+            //User must enter a known word
             if (!words.contains(guess)) {
                 System.out.println("Word was not in list");
                 continue;
             }
+            //Constants for coloring text
             final String ANSI_RESET = "\u001B[0m";
             final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
             final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
             final String ANSI_BLACK_FOREGROUND = "\u001B[30m";
+            //If the user has guessed the entire word
             if (guess.equals(correct)) {
+                //Highlight the entire word green and print it
                 System.out.println(ANSI_GREEN_BACKGROUND + ANSI_BLACK_FOREGROUND + correct + ANSI_RESET);
+                //Set found to true and exit the while loop
                 found = true;
                 continue;
             }
+            //convert guess and correct answer for proper character verification
             char[] guessCharArr = guess.toCharArray();
             char[] correctCharArr = correct.toCharArray();
+            //check each letter to see if it is correct and/or in the correct palce
             for (int i = 0; i < WORD_LENGTH; i++) {
+                //if the letter is not in the correct answer
                 if (guessCharArr[i] != correctCharArr[i] && !correct.contains("" + guessCharArr[i])) {
+                    //print the letter with no highlighting
                     System.out.print(guessCharArr[i]);
                 }
+                //if the letter is in the correct answer but not in the correct position
                 if (guessCharArr[i] != correctCharArr[i] && correct.contains("" + guessCharArr[i])) {
+                    //print the letter in black, highlighted yellow
                     System.out.print(ANSI_YELLOW_BACKGROUND + ANSI_BLACK_FOREGROUND + guessCharArr[i] + ANSI_RESET);
                 }
+                //if the letter is in the correct answer and in the correct position
                 if (guessCharArr[i] == correctCharArr[i]) {
+                    //print the letter in black, highlighted green
                     System.out.print(ANSI_GREEN_BACKGROUND + ANSI_BLACK_FOREGROUND + guessCharArr[i] + ANSI_RESET);
                 }
             }
+            //at the end of each iteration, make a newline
+            //and deduct 1 life.
             System.out.println();
             lives--;
         }
+        //When the loop is terminated, 
+        //close the scanner to prevent memory leakage.
         input.close();
+        //If the user found the correct word
         if (found) {
+            //Congratulate them and end the game.
             System.out.println("Congrats, you won! Game over.");
         }
+        //If not,
         else {
+            //Tell them what the word was.
             System.out.println("The word was: " + correct);
         }
     }
--- WordleTools.java
+++ WordleTools.java
@@ -4,29 +4,57 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Random;
-
+/**
+ * Contains various helper methods used by the main Wordle class.
+ * I kept it here to declutter the JAR's entrypoint.
+ */
 public class WordleTools {
-
+    /**
+     * Converts all words in an {@link ArrayList}<{@link String}> into uppercase
+     * using Java's {@code String.toUppercase} method.
+     * @param list The list to format.
+     * @return The inputted list, but formatted so all strings are uppercase. 
+     */
     public static ArrayList<String> allToUpperCase(ArrayList<String> list) {
+        //Store return value here
         ArrayList<String> out = new ArrayList<>();
-        for (int i = 0; i < list.size(); i++) {
-            out.add(list.get(i).toUpperCase());
-        }
+        //For each entry in the input list, make it uppercase, and add it to the output
+        for (int i = 0; i < list.size(); i++) out.add(list.get(i).toUpperCase());
+        //return the output
         return out;
     }
+    /**
+     * Picks a random value from an {@link ArrayList}<{@link String}>.
+     * @param set The list of {@link String} to choose from.
+     * @return A random {@link String} chosen from the input set.
+     */
     public static String pickRandom(ArrayList<String> set) {
+        //Instantiate an instance of a random number generator
         Random rng = new Random();
+        //Pick a string with index begin a random value from 0 (inclusive) 
+        //to the length of the string. (exclusive)
         return set.get(rng.nextInt(set.size()));
     }
 
+    /**
+     * Takes a list of {@link String}s, and returns all the {@link String}s with length {@code n}.
+     * @param list The list of words to format.
+     * @param n The length of words in the output list
+     * @return An {@link ArrayList}<{@link String}> of all words in {@code list} with length {@code n}.
+     */
     public static ArrayList<String> getNLetterWords(ArrayList<String> list, int n) {
         ArrayList<String> output = new ArrayList<>();
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).length() == n) output.add(list.get(i));
-        }
+        for (int i = 0; i < list.size(); i++) if (list.get(i).length() == n) output.add(list.get(i));
         return output;
     }
 
+    /**
+     * Reads a file using {@code java.nio.file.Files.readAllLines}.
+     * @param fileName The file to read.
+     * @return An {@link ArrayList}<{@link String}> of the contents of the file. It is split on newlines
+     * into the {@link ArrayList}<{@link String}>.
+     * @throws IOException if the input file does not exist.
+     */
     public static ArrayList<String> readFile(String fileName) throws IOException {
         Path name = Paths.get(fileName);
         ArrayList<String> contents = (ArrayList<String>) Files.readAllLines(name);
